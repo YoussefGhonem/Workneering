3a076dc7c22793380962c1aa20e54e590757ecda
gamal fayez <gamalfayez@fortteck.com>
2023-09-09 09:49:03 +0300

    Add get languages drop down

---
+++ workneering/src/Workneering.Settings.Application/Queries/GetLanguages/GetLanguagesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Workneering.Settings.Application.Queries.GetLanguages;
+
+public class GetLanguagesDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string NativeName { get; set; }
+    public string Code { get; set; }
+}
---
+++ workneering/src/Workneering.Settings.Application/Queries/GetLanguages/GetLanguagesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Workneering.Settings.Infrastructure.Persistence;
+
+namespace Workneering.Settings.Application.Queries.GetLanguages;
+
+public class GetLanguagesQueryHandler : IRequestHandler<GetLanguagesQuery, List<GetLanguagesDto>>
+{
+    private readonly SettingsDbContext _context;
+
+    public GetLanguagesQueryHandler(SettingsDbContext context)
+    {
+        _context=context;
+    }
+
+    public async Task<List<GetLanguagesDto>> Handle(GetLanguagesQuery request, CancellationToken cancellationToken)
+    {
+        var languages = await _context.Languages.AsNoTracking().ToListAsync(cancellationToken);
+
+        var dataList = languages.Adapt<List<GetLanguagesDto>>();
+
+        return dataList;
+    }
+}
