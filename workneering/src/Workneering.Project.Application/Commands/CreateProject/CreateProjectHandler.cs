3011f7d7f947ba1c5f619524470d163eae4ef5eb
Youssef Ghonem <youssef.fcaih@gmail.com>
2023-09-25 19:30:13 +0300

    fixes

--- workneering/src/Workneering.Project.Application/Commands/CreateProject/CreateProjectHandler.cs
+++ workneering/src/Workneering.Project.Application/Commands/CreateProject/CreateProjectHandler.cs
@@ -9,21 +9,21 @@ namespace Workneering.Project.Application.Commands.CreateProject
     public class GetFreelancerEducationDetailsQueryHandler : IRequestHandler<CreateProjectCommand, Unit>
     {
         private readonly ProjectsDbContext _context;
-		private readonly IDbQueryService _dbQueryService;
+        private readonly IDbQueryService _dbQueryService;
 
-		public GetFreelancerEducationDetailsQueryHandler(ProjectsDbContext context, IDbQueryService dbQueryService)
-		{
-			_context = context;
-			_dbQueryService = dbQueryService;
-		}
-		public async Task<Unit> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        public GetFreelancerEducationDetailsQueryHandler(ProjectsDbContext context, IDbQueryService dbQueryService)
+        {
+            _context = context;
+            _dbQueryService = dbQueryService;
+        }
+        public async Task<Unit> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
         {
 
-			var categories = _dbQueryService.GetCategoriesForProject(request.categoriesIds);
-			var Supcategoris = _dbQueryService.GetSupCateforiesForProject(request.subCategoriesIds);
-			var skills = _dbQueryService.GetSkillsForProject(request.skillsIds);
+            var categories = _dbQueryService.GetCategoriesForProject(request.CategoriesIds);
+            var Supcategoris = _dbQueryService.GetSupCateforiesForProject(request.SubCategoriesIds);
+            var skills = _dbQueryService.GetSkillsForProject(request.SkillsIds);
 
-			var command = request.CreatProject(categories, Supcategoris, skills);
+            var command = request.CreatProject(categories, Supcategoris, skills);
 
             await _context.Projects.AddAsync(command, cancellationToken);
             _context?.SaveChangesAsync(cancellationToken);
