d6232e7f0d3cba6adcdf1f48116b4dea929f1ba2
Youssef Ghonem <youssef.fcaih@gmail.com>
2023-09-25 21:06:18 +0300

    fixes

---
+++ workneering/src/Workneering.Project.Infrastructure/Migrations/20230925180613_updateFields.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workneering.Project.Infrastructure.Migrations
+{
+    public partial class updateFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AssginedFreelancerId",
+                schema: "ProjectsSchema",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssginedFreelancerId",
+                schema: "ProjectsSchema",
+                table: "Projects");
+        }
+    }
+}
