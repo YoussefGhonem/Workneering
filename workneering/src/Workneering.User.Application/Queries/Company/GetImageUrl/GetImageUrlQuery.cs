3bd99db8a3024867a2f0d0596ea41359e2fa4400
Amr <amrshrif53@gmail.com>
2023-10-01 15:28:08 +0300

    Profile Image TopBar get By Key In Aws

---
+++ workneering/src/Workneering.User.Application/Queries/Company/GetImageUrl/GetImageUrlQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workneering.User.Application.Queries.Client.GetClientCategorization;
+
+namespace Workneering.User.Application.Queries.Company.GetImageUrl
+{
+    public class GetImageUrlQuery : IRequest<GetImageUrlDto>
+    {
+        public Guid ClientId { get; set; }
+    }
+}
---
+++ workneering/src/Workneering.User.Application/Queries/Freelancer/GetImageUrl/GetImageUrlDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workneering.User.Application.Queries.Freelancer.GetImageUrl
+{
+    public class GetImageUrlDto
+    {
+        public string ImageUrl { get; set;}
+    }
+}
