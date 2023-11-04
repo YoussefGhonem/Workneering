13519920f47ada274123144dfd1f22acd0dfe7a6
Youssef Ghonem <youssef.fcaih@gmail.com>
2023-11-04 18:29:51 +0200

    fizes

--- workneering/src/Workneering.Base.API/ServiceCollections/Serilog/ServiceCollectionExtension.cs
+++ workneering/src/Workneering.Base.API/ServiceCollections/Serilog/ServiceCollectionExtension.cs
@@ -3,25 +3,32 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
+using System.Configuration;
 
 namespace Workneering.Base.API.ServiceCollections.Serilog;
 public static class ServiceCollectionExtension
 {
     public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder, ILoggingBuilder loggingBuilder)
     {
-
-
-        var logger = new LoggerConfiguration()
-          .ReadFrom.Configuration(builder.Configuration)
-          .Enrich.FromLogContext()
-          .CreateLogger();
-        builder.Logging.ClearProviders();
-        builder.Logging.AddSerilog(logger);
+        //var logger = new LoggerConfiguration()
+        //     .ReadFrom.Configuration(builder.Configuration)
+        //     .Enrich.FromLogContext()
+        //     .WriteTo.MSSqlServer(
+        //        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+        //        sinkOptions: new MSSqlServerSinkOptions
+        //        {
+        //            TableName = "dbo.Logs",
+        //            AutoCreateSqlTable = true, // Creates the table if it doesn't exist
+        //        })
+        //  .CreateLogger();
+        //// builder.Host.UseSerilog();
+        //loggingBuilder.ClearProviders();
+        //loggingBuilder.AddSerilog(logger);
         return builder;
     }
-    public static WebApplication UseSerilog(this WebApplication app)
+    public static WebApplication UseHealthCheckApplication(this WebApplication app)
     {
-        // app.UseSerilogRequestLogging();
+        app.UseSerilogRequestLogging();
         return app;
     }
 }
